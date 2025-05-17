ab2f3ff30b56000163c0a1bacf183bc650fc0fd1
shiv127001 <shiv127001@gmail.com>
2025-05-17 13:10:33 +0530

    date 17-05-2025

---
+++ Lec_89 Maximum Path Sum in Binary Tree.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<map>
+#include<set>
+using namespace std;
+
+typedef struct node{
+    int data ;
+    struct node* left;
+    struct node* right;
+}node;
+
+    int pathsum(node* root, int &maxi)
+    {
+        if (root == NULL){return 0;}
+
+        int lsum = max(0,pathsum(root->left,maxi));
+        int rsum = max(0,pathsum(root->right, maxi));
+        maxi = max(maxi, lsum+rsum+root->data);
+        return root->data+max(lsum,rsum);
+    }
+
+    int maxPathSum(node* root) {
+        int maxi =INT_MIN;
+        int sum = pathsum(root,maxi);
+        return maxi;
+    }
