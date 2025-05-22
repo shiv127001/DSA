49eab7ed7e3948fbf5433d81c6ed5a5638122269
shiv127001 <shiv127001@gmail.com>
2025-05-22 16:38:37 +0530

    date 22 may 2025

--- Lec_91.ZigZagTraversal.cpp
+++ Lec_91.ZigZagTraversal.cpp
@@ -1,15 +1,16 @@
-#include<iostream>
-#include<vector>
-#include<stack>
-#include<queue>
-#include<map>
-#include<set>
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
 using namespace std;
 
-typedef struct node{
-    int data ;
-    struct node* left;
-    struct node* right;
+typedef struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
 
     node(int val)
     {
@@ -17,36 +18,36 @@ typedef struct node{
         left = right = NULL;
     }
 
-}node;
+} node;
 
-node* buildtree()
+node *buildtree()
 {
     int value;
     int value1;
     int value2;
-    cout <<"Enter the value for node (-1 for NULL)";
-    cin>>value;
+    cout << "Enter the value for node (-1 for NULL)";
+    cin >> value;
 
-    node* root = new node(value);
-    queue<node*> q;
+    node *root = new node(value);
+    queue<node *> q;
     q.push(root);
-    
-    while(!q.empty())
+
+    while (!q.empty())
     {
-        node* current = q.front();
+        node *current = q.front();
         q.pop();
 
-        cout<<"Enter the left child for "<<current->data<<" (-1 for NULL)";
-        cin>>value1;
-        if(value1 != -1)
+        cout << "Enter the left child for " << current->data << " (-1 for NULL)";
+        cin >> value1;
+        if (value1 != -1)
         {
             current->left = new node(value1);
             q.push(current->left);
         }
 
-        cout<<"Enter the right child for "<<current->data<<" (-1 for NULL)";
-        cin>>value2;
-        if(value2 != -1)
+        cout << "Enter the right child for " << current->data << " (-1 for NULL)";
+        cin >> value2;
+        if (value2 != -1)
         {
             current->right = new node(value2);
             q.push(current->right);
@@ -55,23 +56,60 @@ node* buildtree()
     return root;
 }
 
-vector<vector<int>> ZigZagTraversal(node* tree)
+vector<vector<int>> zigzagLevelOrder(node *root)
 {
-
+    if (root == NULL)
+    {
+        return {};
+    }
+    bool lefttoright = true;
+    vector<vector<int>> result;
+    queue<node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        int size = q.size();
+        vector<int> row(size);
+        for (int i = 0; i < size; ++i)
+        {
+            node *current = q.front();
+            q.pop();
+            int index;
+            if (lefttoright)
+            {
+                index = i;
+            }
+            else
+            {
+                index = size - 1 - i;
+            }
+            row[index] = current->data;
+            if (current->left)
+            {
+                q.push(current->left);
+            }
+            if (current->right)
+            {
+                q.push(current->right);
+            }
+        }
+        lefttoright = !lefttoright;
+        result.push_back(row);
+    }
+    return result;
 }
-
 int main()
 {
-    cout<<"Tree1";
-    node* t1 = buildtree();
-    vector<vector<int>>a = ZigZagTraversal(t1);
-    for(int i=0;i<a.size();++i)
+    cout << "Tree1";
+    node *t1 = buildtree();
+    vector<vector<int>> a = zigzagLevelOrder(t1);
+    for (int i = 0; i < a.size(); ++i)
     {
-        for(int j=0;j<a[i].size();++i)
+        for (int j = 0; j < a[i].size(); ++i)
         {
-            cout<<a[i][j]<<", ";
+            cout << a[i][j] << ", ";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
