7dea0ed9ac966d663535f829898659050d693239
shiv127001 <shiv127001@gmail.com>
2025-05-28 11:57:16 +0530

    date 28 may 2025

---
+++ Lec_98 Print Root to Node Path in Binary Tree.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+using namespace std;
+
+typedef struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+
+    node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+
+} node;
+
+node *buildtree()
+{
+    int value;
+    int value1;
+    int value2;
+    cout << "Enter the value for node (-1 for NULL)";
+    cin >> value;
+
+    node *root = new node(value);
+    queue<node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        node *current = q.front();
+        q.pop();
+
+        cout << "Enter the left child for " << current->data << " (-1 for NULL)";
+        cin >> value1;
+        if (value1 != -1)
+        {
+            current->left = new node(value1);
+            q.push(current->left);
+        }
+
+        cout << "Enter the right child for " << current->data << " (-1 for NULL)";
+        cin >> value2;
+        if (value2 != -1)
+        {
+            current->right = new node(value2);
+            q.push(current->right);
+        }
+    }
+    return root;
+}
