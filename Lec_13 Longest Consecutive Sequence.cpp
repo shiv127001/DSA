a7f6851d9097b93958c3e8c5f63944bb2759bd52
shiv127001 <shiv127001@gmail.com>
2025-06-22 01:51:13 +0530

    date 22 june 2025

--- Lec_13 Longest Consecutive Sequence.cpp
+++ Lec_13 Longest Consecutive Sequence.cpp
@@ -1,4 +1,33 @@
-# include<iostream>
-# include<sstream>
-# include<string>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm> // header file for sort()
 using namespace std;
+
+int main()
+{
+    vector<int> arr = {1, 2, 100, 101,103,104, 2, 1, 3, 3, 4, 101, 102};
+    sort(arr.begin(), arr.end());
+    int currcount = 0;
+    int lastsmaller = -1;
+    int longest = 1, n = arr.size();
+    for (int i = 0; i < n; ++i)
+    {
+        if (lastsmaller == arr[i] - 1)
+        {
+            currcount++;
+            lastsmaller = arr[i];
+        }
+        else if (lastsmaller != arr[i])
+        {
+            currcount = 1;
+            lastsmaller = arr[i];
+        }
+        // cout << "currcount : " << currcount << ", " << endl;
+        // cout << "lastsmaller: " << lastsmaller << ", " << endl;
+        longest = max(longest, currcount);
+    }
+    cout << "LCS : " << longest;
+    return 0;
+}
