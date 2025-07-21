bdd97121b99417502a2a92b9d0c0588b4565cbcd
shiv127001 <shiv127001@gmail.com>
2025-07-21 13:51:29 +0530

    LCS

--- Lec_29 Longest Consecutive sequence.cpp
+++ Lec_29 Longest Consecutive sequence.cpp
@@ -7,66 +7,66 @@
 #include <algorithm> // header file for sort()
 using namespace std;
 
-bool linearsearch(vector<int> a,int x)
+bool linearsearch(vector<int> a, int x)
 {
     int n = a.size();
-    for(int i=0;i<n;++i)
+    for (int i = 0; i < n; ++i)
     {
-        if(a[i] == x) return true;
+        if (a[i] == x)
+            return true;
     }
     return false;
 }
 
-void LCS_Optimal(vector<int>arr,int n)
+void LCS_Optimal(vector<int> arr, int n)
 {
-    if(n==0) return ;
+    if (n == 0)
+        return;
     int longest = 1;
     unordered_set<int> st;
-    for (int i=0;i<n;++i)
+    for (int i = 0; i < n; ++i)
     {
         st.insert(arr[i]);
     }
-    for(auto it:st)
+    for (auto it : st)
     {
-        if(st.find(it-1)==st.end())
+        if (st.find(it - 1) == st.end())
         {
-            int cnt =1;
+            int counter = 1;
             int x = it;
-            while(st.find(x+1)!=st.end())
+            while (st.find(x + 1) != st.end())
             {
-                cnt++;
+                counter++;
                 x++;
-
             }
-            longest = max(longest,cnt);
+            longest = max(longest, counter);
         }
     }
-    cout<<"LCS with optimal : " <<longest;
+    cout << "LCS with optimal : " << longest;
 }
 
 int main()
 {
-    vector<int> arr ={100,1,2,101,102,3,4,8,9,10,11,12,13,14};
+    vector<int> arr = {100, 1, 2, 101, 102, 3, 4, 8, 9, 10, 11, 12, 13, 14};
     int counter = 1;
     int longest = 1;
-    int i,j;
+    int i, j;
     int n = arr.size();
-    for(i=0;i<n;++i)
+    for (i = 0; i < n; ++i)
     {
-        int x =  arr[i];
+        int x = arr[i];
         counter = 1;
-        while(linearsearch(arr,x+1)==true)
+        while (linearsearch(arr, x + 1) == true)
         {
             counter++;
             x++;
         }
-        longest = max(counter,longest);
+        longest = max(counter, longest);
     }
-    cout<<"Longest consecutive sequence is : "<<longest;
+    cout << "Longest consecutive sequence is : " << longest;
 
-    cout<<"\n ----\n Optimal approach :\n";
-    LCS_Optimal(arr,n);
+    cout << "\n ----\n Optimal approach :\n";
+    LCS_Optimal(arr, n);
 
     return 0;
-
 }
