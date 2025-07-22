1f8f9628a735c79480424c7660e1cd6aa52633c5
shiv127001 <shiv127001@gmail.com>
2025-07-22 20:17:03 +0530

    Longest subarray with sum k

--- Lec_4 Longest Subarray with sum K .cpp
+++ Lec_4 Longest Subarray with sum K .cpp
@@ -1,9 +1,54 @@
 #include <iostream>
-#include <sstream>
+#include <unordered_map>
 #include <string>
 #include <vector>
 #include <algorithm> // header file for sort()
 using namespace std;
+
+int longestSubarrayWithSumK_BRUTE_FORCE(vector<int> arr, int n, int k)
+{
+    cout<<"\n---BRUTE_FORCE---\n";
+    int i, j;
+    int longest = 0;
+    for (i = 0; i < n; ++i)
+    {
+        int sum = 0;
+        for (j = i; j < n; ++j)
+        {
+            sum += arr[j];
+            if (sum == k)
+                longest = max(longest, j - i + 1);
+        }
+    }
+    return longest;
+}
+
+int longestSubarrayWithSumK_OPTIMAL(vector<int> arr, int n, int k)
+{
+    cout<<"\n---OPTIMAL ONE---\n";
+    int i, longest = 0, sum = 0;
+    unordered_map<int, int> mpp;
+    for (i = 0; i < n; ++i)
+    {
+        sum += arr[i];
+        if (sum == k)
+        {
+            longest = max(longest, i + 1);
+        }
+        
+        int remain = sum - k;
+        if (mpp.find(remain) != mpp.end())
+        {
+            int len = i - mpp[remain];
+            longest = max(longest, len);
+        }
+        if (mpp.find(sum) == mpp.end())
+        {
+            mpp[sum] = i;
+        }
+    }
+    return longest;
+}
 
 int longestSubarrayWithSumK(vector<int> arr, int n, int k)
 {
@@ -12,17 +57,19 @@ int longestSubarrayWithSumK(vector<int> arr, int n, int k)
     int longest = 0;
     for (int i = 0; i < n; ++i)
     {
-        while(a<b && sum>k)
+        while (a < b && sum > k)
         {
-            sum-= arr[a];
+            sum -= arr[a];
             a++;
         }
-        if(sum == k) {
-            longest = max(longest,b-a+1);
+        if (sum == k)
+        {
+            longest = max(longest, b - a + 1);
         }
         b++;
-        if(b<n){
-            sum+=arr[b];
+        if (b < n)
+        {
+            sum += arr[b];
         }
     }
     return longest;
@@ -30,9 +77,11 @@ int longestSubarrayWithSumK(vector<int> arr, int n, int k)
 
 int main()
 {
-    vector<int> arr = {1, 2, 3, 1, 1, 1,1};
+    vector<int> arr = {1, 2, 3, 1, 1, 1, 1};
     int k = 4, n = 7;
-    int sumk = longestSubarrayWithSumK(arr, n, k);
-    cout<<"Longest Sum = " << sumk;
+    int sumk_with_BRUTE_FORCE = longestSubarrayWithSumK_BRUTE_FORCE(arr, n, k);
+    cout << "Longest Subarray length (BRUTE FORCE) = " << sumk_with_BRUTE_FORCE;
+    int sumk_with_optimal = longestSubarrayWithSumK_OPTIMAL(arr, n, k);
+    cout << "Longest Subarray length (OPTIMAL) = " << sumk_with_optimal;
     return 0;
 }
