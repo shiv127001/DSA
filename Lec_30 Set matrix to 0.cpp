38a48859e1a9578960179755cdada9b03a94db7d
shiv127001 <shiv127001@gmail.com>
2025-07-26 20:10:19 +0530

    Set matrix 0 by better appraoch

--- Lec_30 Set matrix to 0.cpp
+++ Lec_30 Set matrix to 0.cpp
@@ -5,9 +5,55 @@
 #include <climits> // For INT_MAX
 using namespace std;
 
-class Solution
+class Solution_better {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int i,j;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<int>k(m,0);
+        vector<int>p(n,0);
+        for(i=0;i<n;++i)
+        {
+            for(j=0;j<m;++j)
+            {
+                if(matrix[i][j] == 0)
+                {
+                    k[j]=1;
+                    p[i]=1;
+                }
+            }
+        }
+        for(int s=0;s<m;++s)
+        {
+            if(k[s]==1)
+            {
+                j=s;
+                for(i=0;i<n;++i)
+                {
+                    matrix[i][j] =0;
+                }
+            }
+        }
+
+        for(int r = 0;r<n;++r)
+        {
+            if(p[r] == 1)
+            {
+                i=r;
+                for(j=0;j<m;++j)
+                {
+                    matrix[i][j] =0;
+                }
+            }
+        }
+    }
+};
+
+class Solution // brute force
 {
 public:
+    // brute force
     // Time Complexity: O((N*M)*(N + M)) + O(N*M), where N = no. of rows in the matrix and M = no. of columns in the matrix.
     void setRow(vector<vector<int>> &matrix, int n, int m, int i)
     {
@@ -64,8 +110,12 @@ public:
 int main()
 {
     vector<vector<int>> matrix;
-    Solution myobj;
+
+    Solution myobj; // brute force
     myobj.setZeroes(matrix);
+
+    Solution_better myobj2;
+    myobj2.setZeroes(matrix);
     return 0;
 
 }
